e7a415e023bcbaede709b17288a93a6427e184fc
maiconm <maiconm23@gmail.com>
2020-10-10 16:00:54 -0300

    IMC implementation

--- app/src/main/java/com/example/calculaimc/MainActivity.java
+++ app/src/main/java/com/example/calculaimc/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.calculaimc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText peso;
+    EditText altura;
+    TextView imcResult;
+    Button btnCalcular;
+    Button btnLimpar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        peso = findViewById(R.id.peso);
+        altura = findViewById(R.id.altura);
+        imcResult = findViewById(R.id.imcResult);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        btnLimpar = findViewById(R.id.btnLimpar);
+    }
+
+    public void calcular(View v) {
+        try {
+            Double valorPeso = Double.parseDouble(peso.getText().toString());
+            Double valorAltura = Double.parseDouble(altura.getText().toString());
+            Double imc = valorPeso / (Math.pow(valorAltura, 2));
+            imcResult.setText(imc.toString());
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            Toast.makeText(this, "Informe valores validos", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void limpar(View v) {
+        peso.getText().clear();
+        altura.getText().clear();
+        imcResult.setText("");
     }
 }
